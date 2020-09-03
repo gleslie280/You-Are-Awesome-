5845009314f46d69c0eb2661d83a87797ad455b7
Celeste Leslie <giovannamae12@gmail.com>
2020-09-02 20:55:41 -0600

    Completed 2.1

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Jo Leslie on 9/2/20.
+//  Copyright © 2020 Jo Leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
