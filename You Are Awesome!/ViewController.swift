4015b6b8c9c8a1c07bc507e62c407ddda7154af7
Jo Leslie <gleslie@zagmail.gonzaga.edu>
2020-09-14 17:56:44 -0600

    2.13 loops and repeat

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var imageNumber = 0
-    var messageNumber = 0
+    var imageNumber = -1
+    var messageNumber = -1
     let totalNumberOfImages = 9
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,38 +29,26 @@ class ViewController: UIViewController {
                         "When the Genius Bar Needs Help, They Call You!",
                         "Fabulous? That's You!",
                         "You've Got the Design Skills of Jony Ive"]
-        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
-        imageView.image = UIImage(named:"image\(Int.random(in: 0...9))")
+    
+
+        var newMessageNumber: Int
+        repeat
+        {
+            newMessageNumber = Int.random(in: 0...messages.count-1)
+        } while messageNumber == newMessageNumber
+      messageNumber = newMessageNumber
+        messageLabel.text = messages[messageNumber]
+
+        var newImageNumber: Int
+        repeat {
+            newImageNumber = Int.random(in: 0...totalNumberOfImages)
+    } while imageNumber == newImageNumber
+        imageNumber = newImageNumber
+        imageView.image = UIImage(named: "image\(imageNumber)")
         
         
-    //    messageLabel.text = messages[messageNumber]
-    //    messageNumber += 1
-    //    if messageNumber == messages.count {
-    //    messageNumber = 0
-    //   }
-        
-        
-    //    let imageName = "image" + String(imageNumber)
-    //    imageView.image = UIImage(named: imageName)
-   //     imageNumber = imageNumber + 1
-   //     if imageNumber == 10 {
-   //         imageNumber = 0
-   //     }
-                
-//        let awesomeMessage = "You Are Awesome!"
-//        let greatMessage = "You Are Great!"
-//        let bombMessage = "You Are Da Bomb!"
-//
-//        if messageLabel.text == awesomeMessage {
-//        messageLabel.text = greatMessage
-//        imageView.image = UIImage(named: "image1")
-//        } else if messageLabel.text == greatMessage{
-//            messageLabel.text = bombMessage
-//            imageView.image = UIImage(named: "image2")
-//        } else{
-//            messageLabel.text = awesomeMessage
-//            imageView.image = UIImage(named: "image0")
-//        }
-}
+    }
+
 
 }
+
